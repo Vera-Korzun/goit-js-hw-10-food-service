6483ba356c849c87244f1b7ad528328141363ff2
Vera Korzun <veraminich@gmail.com>
2020-12-07 13:39:33 +0100

    Added changes

--- src/index.js
+++ src/index.js
@@ -2,36 +2,34 @@
 import data from './data/menu.json';
 import cardsTemplate from './templates/cardsTemplate.hbs';
 import './styles.css';
-
 //console.log(cardsTemplate(data));
 
 const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
   };
-
 const getUl=document.querySelector('.menu');
 const checkboxReference = document.querySelector('.theme-switch__toggle');
 const getBody = document.querySelector('body');
 
 const onDarkTheme = () => {
-    getBody.classList.contains('light-theme') && getBody.classList.remove('light-theme');
-    getBody.classList.add('dark-theme');
+    getBody.classList.remove(Theme.LIGHT);
+    getBody.classList.add(Theme.DARK);
 };
 
 const onLighTheme = () => {
-    getBody.classList.contains('dark-theme') && getBody.classList.remove('dark-theme');
-    getBody.classList.add('light-theme');
+    getBody.classList.remove(Theme.DARK);
+    getBody.classList.add(Theme.LIGHT);
 };
 
-if (localStorage.getItem('theme')) {
-    const checked = JSON.parse(localStorage.getItem('theme'));
+if (localStorage.getItem(Theme)) {
+    const checked = JSON.parse(localStorage.getItem(Theme));
     checkboxReference.checked = checked;
     checked ? onDarkTheme() : onLighTheme();
 }
 
 const checkboxState = (e) => {
-    localStorage.setItem('theme', JSON.stringify(e.target.checked))
+    localStorage.setItem(Theme, JSON.stringify(e.target.checked))
     e.target.checked ? onDarkTheme() : onLighTheme();
 };
 
